a451044ec12f675ed062c8ee06aa4d650bcab69b
charwaii <61100546+charwaii@users.noreply.github.com>
2021-08-31 02:12:35 +1000

    update

--- src/layouts/headerPopup.js
+++ src/layouts/headerPopup.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
 const HeaderPopup = (props) => {
+  const [showCv, setShowCv] = useState(false);
+
+  const showCvHandler = (prev) => {
+    if (showCv) {
+      setShowCv(false);
+    } else {
+      setShowCv(true);
+    }
+  };
+
   const { about } = useStaticQuery(
     graphql`
       query MyQuery {
@@ -29,40 +39,55 @@ const HeaderPopup = (props) => {
   return (
     <div className="header-info">
       <div className="popup-buttons">
-        <button onClick={props.showNavHandler} className="btn">
-          [close]
-        </button>
+        <div>
+          <button onClick={showCvHandler} className="btn">
+            {!showCv ? "[cv]" : "[about]"}
+          </button>
+          <button onClick={props.showNavHandler} className="btn">
+            [close]
+          </button>
+        </div>
       </div>
+      {!showCv && (
+        <div className="popup-info">
+          <p>E: {about.edges[0].node.frontmatter.email}</p>
+          <div className="ig">
+            <span>IG: </span>
+            <a
+              href="https://instagram.com/nuanhoart"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              {about.edges[0].node.frontmatter.instagram}
+            </a>
+          </div>
+          <p>Location: {about.edges[0].node.frontmatter.location}</p>
 
-      <p>E: {about.edges[0].node.frontmatter.email}</p>
-      <div className="ig">
-        <span>IG: </span>
-        <a
-          href="https://instagram.com/nuanhoart"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          {about.edges[0].node.frontmatter.instagram}
-        </a>
-      </div>
-      <p>Location: {about.edges[0].node.frontmatter.location}</p>
+          <p className="bio">{about.edges[0].node.frontmatter.bio}</p>
+        </div>
+      )}
 
-      <p className="bio">{about.edges[0].node.frontmatter.bio}</p>
-      <div className="cv">
-        <p>2019</p>
-        <p>
-          '(De)faced', <b>Sheffer Gallery</b>, Sydney
-        </p>
-        <p>2019</p>
-        <p>
-          '(De)faced', <b>Sheffer Gallery</b>, Sydney
-        </p>
-        <p>2019</p>
-        <p>
-          '(De)faced', <b>Sheffer Gallery</b>, Sydney
-        </p>
-      </div>
+      {showCv && (
+        <div className="cv">
+          <p>2019</p>
+          <p>
+            (De)faced, <b>Sheffer Gallery</b>, Sydney
+          </p>
+          <p>2019</p>
+          <p>
+            2019 Invitational, <b>Home735 Gallery</b>, Sydney
+          </p>
+          <p>2019</p>
+          <p>
+            Group Show, <b>Home735 Gallery</b>, Sydney
+          </p>
+          <p>2019</p>
+          <p>
+            Accelerating the Fractured Whereabouts, <b>Freda's</b>, Sydney
+          </p>
+        </div>
+      )}
       <p className="copyright">Nuan Ho © 2021 - Site by NakedLunch</p>
     </div>
   );
