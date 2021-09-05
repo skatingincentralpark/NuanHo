9ff4db3539b75e301f61e0f5ef4141ee75969da0
charwaii <61100546+charwaii@users.noreply.github.com>
2021-09-05 15:21:17 +1000

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,8 +73,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        name: `Nuan Ho`,
+        short_name: `Nuan Ho`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
