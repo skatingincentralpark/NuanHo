e71a83fe7d3f0690c1bbb0ebfba84c3c2adcfa0b
charwaii <61100546+charwaii@users.noreply.github.com>
2021-09-29 17:07:46 +1000

    changed footer link to paragraph

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby";
 const footer = () => {
   return (
     <footer>
-      <Link to="www.instagram.com">Nuan Ho © 2021 - Site by NakedLunch</Link>
+      <p>Nuan Ho © 2021 - Site by NakedLunch</p>
     </footer>
   );
 };
